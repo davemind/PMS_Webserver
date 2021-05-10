298a7c1ba9ea05a5960639cb2b0e376c21c544c1
clever-knowledge <victoryabc.tianxiaowudi@outlook.com>
2021-05-10 23:30:03 +0800

    login with email & password

--- engine.py
+++ engine.py
@@ -8,7 +8,7 @@ def check_password(db_password, login_password):
 
 from sql import get_one_record
 def log_in(user_name, password):
-	sql_command = 'select `password`, `role_id`, `id`  from `tbl_admin` where `name` = %s'
+	sql_command = 'select `password`, `role_id`, `id`  from `tbl_admin` where `email` = %s'
 	num, info = get_one_record(sql_command, user_name)
 	if num < 1: return error_400_message("user name")
 	if not check_password(info[0], password):
