c88a5bdf1a3a371022d187eaa55a3beeccc22326
pang <devforupwork@gmail.com>
2021-05-12 19:46:49 +0800

    Update Camera_View.js

--- static/scripts/pages/Camera_View.js
+++ static/scripts/pages/Camera_View.js
@@ -12,12 +12,13 @@ function myFunction() {
 		},
 		success: function (result) {
 			st_index = 0;
-			var address = location.protocol + '//' + location.host + ':5001/video_feed';
+			var address = window.location.protocol + "//" + window.location.hostname + ":5001/video_feed";
 			cameras = JSON.parse(result);
 			for (var i = st_index; i < Math.min(cameras.length, st_index + 6); ++i) {
 				var j = i % 6 + 1;
 				document.getElementById("camera_name" + j.toString()).textContent = cameras[i].camera_name;
-				$("#camera_url" + j.toString()).attr("src", address + j.toString() + "?url=" + cameras[i].camera_url)
+				$("#camera_url" + j.toString()).attr("src", address + j.toString() + "?url=" + cameras[i].camera_url);
+
 			}
 		}
 	});	
