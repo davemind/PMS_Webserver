29388f4148c03d442d9b6e6b8b3df98f10fcd9e9
pang <devforupwork@gmail.com>
2021-05-24 19:54:37 +0800

    add grid sql

---
+++ Website/db_backup/grid.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.1.32-MariaDB : Database - vms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`vms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `vms`;
+
+/*Table structure for table `grids` */
+
+DROP TABLE IF EXISTS `grids`;
+
+CREATE TABLE `grids` (
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `rows` int(2) unsigned NOT NULL DEFAULT '2',
+  `cols` int(2) unsigned NOT NULL DEFAULT '3',
+  `cameras` longtext,
+  `user_id` int(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `grids` */
+
+insert  into `grids`(`id`,`rows`,`cols`,`cameras`,`user_id`) values (1,4,5,'stef,container1,container2,container3',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
