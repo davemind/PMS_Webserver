3d29b1b02e7ce3ae5c008b4304262ddbefdcdfd3
pang <devforupwork@gmail.com>
2021-05-26 15:18:18 +0800

    add schedule

--- VideoStreaming/bin/camera_manage.py
+++ VideoStreaming/bin/camera_manage.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, json, session, Response
 from responses import error_400_message, error_401_message, error_403_message, success_200_message
 from datetime import timedelta
 import subprocess
+import schedule
+import time
 
 app = Flask(__name__)
 
@@ -18,7 +20,27 @@ for camera in cameras:
 		proc = subprocess.Popen(command)
 		proc_dict[camera['id']] = proc
 
+def playCamera():
+	sql_command = 'select `id`, `camera_name`, `camera_url`, `server_url`, `state`, `location`,`user_id` from `cameras`'
+	cameras = get_full_data(sql_command)
+	for camera in cameras:
+		if camera['state'] == 1:
+			if camera['id'] in proc_dict:
+				if proc_dict[camera['id']] is None:
+					command = 'gst-launch-1.0.exe rtspsrc protocols=tcp location="' + camera['camera_url'] + '" ! rtph264depay ! h264parse ! mpegtsmux ! hlssink location=' + camera['camera_name'] + '.%05d.ts playlist-location=' + camera['camera_name'] + '.m3u8'
+					proc = subprocess.Popen(command)
+					proc_dict[camera['id']] = proc
+			else:
+				command = 'gst-launch-1.0.exe rtspsrc protocols=tcp location="' + camera['camera_url'] + '" ! rtph264depay ! h264parse ! mpegtsmux ! hlssink location=' + camera['camera_name'] + '.%05d.ts playlist-location=' + camera['camera_name'] + '.m3u8'
+				proc = subprocess.Popen(command)
+				proc_dict[camera['id']] = proc
+		else:
+			if camera['id'] in proc_dict:
+				if proc_dict[camera['id']] is not None:
+					proc_dict[camera['id']].kill()
+					proc_dict[camera['id']] = None
 
+					
 @app.route('/api/playAllCamera', methods=['POST'])
 def playAllCamera():
 	sql_command = 'select `id`, `camera_name`, `camera_url`, `server_url`, `state`, `location`,`user_id` from `cameras`'
@@ -74,5 +96,12 @@ def disconnectCamera():
 			proc_dict[camera_id].kill()
 			proc_dict[camera_id] = None
 	return json.dumps({'code': 200})
+	
 if __name__ == "__main__":
-	app.run(debug=False, host='0.0.0.0', port=5001, threaded=True)
+	schedule.every(10).seconds.do(playCamera)
+	while True:
+		# Checks whether a scheduled task 
+		# is pending to run or not
+		schedule.run_pending()
+		time.sleep(5)
+	#app.run(debug=False, host='0.0.0.0', port=5001, threaded=True)
